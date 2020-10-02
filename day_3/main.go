de7ddfebd3cdd46fd797962cfaace3f028916627
Olena23 <tajlintajlin@gmail.com>
2020-10-03 00:01:34 +0300

    improve naming and readability

--- day_3/main.go
+++ day_3/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
-	"strings"
 	"strconv"
-	"fmt"
+	"strings"
 )
 
 func main() {
@@ -15,40 +15,39 @@ func main() {
 		log.Fatal(err)
 	}
 	defer file.Close()
+
 	var sum = 0
 	var fabric [4000][4000]int
-	var allStrings []string
+	allStrings := make([]string, 0)
 	sc := bufio.NewScanner(file)
 	for sc.Scan() {
 		newStr := sc.Text()
 		allStrings = append(allStrings, newStr)
 	}
 
-	for i := range allStrings {
-		one := strings.Split(allStrings[i], " ")
-		fromLeft := strings.Split(one[2], ",")[0]
-		fromLeftInt, _ := strconv.Atoi(fromLeft)
-		fromTop := strings.Split(one[2], ",")[1]
-		fromTop = fromTop[0:len(fromTop)-1]
-		fromTopInt, _ := strconv.Atoi(fromTop)
-		width := strings.Split(one[3], "x")[0]
-		widthInt, _ := strconv.Atoi(width)
-		height := strings.Split(one[3], "x")[1]
-		heightInt, _ := strconv.Atoi(height)
+	for _, str := range allStrings {
+		words := strings.Split(str, " ")
 
-		for m := 0; m < heightInt; m++ {
-			for k := 0; k < widthInt; k++ {
-				if fabric[fromTopInt + m][fromLeftInt + k] == 1 {
+		padding := strings.Split(words[2], ",")
+		fromLeft, _ := strconv.Atoi(padding[0])
+		fromTop, _ := strconv.Atoi(strings.TrimSuffix(padding[1], ":"))
+
+		dimensions := strings.Split(words[3], "x")
+		width, _ := strconv.Atoi(dimensions[0])
+		height, _ := strconv.Atoi(dimensions[1])
+
+		for i := 0; i < height; i++ {
+			for j := 0; j < width; j++ {
+				if fabric[fromTop+i][fromLeft+j] == 1 {
 					sum++
-					fabric[fromTopInt+ m][fromLeftInt + k] = 2
-				} else if fabric[fromTopInt+ m][fromLeftInt + k] == 2 {
+					fabric[fromTop+i][fromLeft+j] = 2
+				} else if fabric[fromTop+i][fromLeft+j] == 2 {
 					continue
 				} else {
-					fabric[fromTopInt+ m][fromLeftInt + k] = 1
+					fabric[fromTop+i][fromLeft+j] = 1
 				}
 			}
 		}
-
 
 	}
 
@@ -57,81 +56,75 @@ func main() {
 	part2()
 }
 
-func part2 ()  {
+func part2() {
 
 	file, err := os.Open("day_3/input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
+
 	var sum = 0
 	var fabric [4000][4000]int
-	var allStrings []string
+	allStrings := make([]string, 0)
 	sc := bufio.NewScanner(file)
 	for sc.Scan() {
 		newStr := sc.Text()
 		allStrings = append(allStrings, newStr)
 	}
 
-	for i := range allStrings {
-		one := strings.Split(allStrings[i], " ")
-		fromLeft := strings.Split(one[2], ",")[0]
-		fromLeftInt, _ := strconv.Atoi(fromLeft)
-		fromTop := strings.Split(one[2], ",")[1]
-		fromTop = fromTop[0:len(fromTop)-1]
-		fromTopInt, _ := strconv.Atoi(fromTop)
-		width := strings.Split(one[3], "x")[0]
-		widthInt, _ := strconv.Atoi(width)
-		height := strings.Split(one[3], "x")[1]
-		heightInt, _ := strconv.Atoi(height)
+	for _, str := range allStrings {
+		words := strings.Split(str, " ")
+
+		padding := strings.Split(words[2], ",")
+		fromLeft, _ := strconv.Atoi(padding[0])
+		fromTop, _ := strconv.Atoi(strings.TrimSuffix(padding[1], ":"))
+
+		dimensions := strings.Split(words[3], "x")
+		width, _ := strconv.Atoi(dimensions[0])
+		height, _ := strconv.Atoi(dimensions[1])
 
 		sumOfsquares := 0
-		for m := 0; m < heightInt; m++ {
-			for k := 0; k < widthInt; k++ {
-				if fabric[fromTopInt + m][fromLeftInt + k] == 1 {
+		for m := 0; m < height; m++ {
+			for k := 0; k < width; k++ {
+				if fabric[fromTop+m][fromLeft+k] == 1 {
 					sum++
-					fabric[fromTopInt+ m][fromLeftInt + k] = 2
-				} else if fabric[fromTopInt+ m][fromLeftInt + k] == 2 {
+					fabric[fromTop+m][fromLeft+k] = 2
+				} else if fabric[fromTop+m][fromLeft+k] == 2 {
 					continue
 				} else {
-					fabric[fromTopInt+ m][fromLeftInt + k] = 1
-					sumOfsquares ++
+					fabric[fromTop+m][fromLeft+k] = 1
+					sumOfsquares++
 				}
 			}
 		}
-		}
+	}
 
+	for _, str := range allStrings {
+		words := strings.Split(str, " ")
 
+		padding := strings.Split(words[2], ",")
+		fromLeft, _ := strconv.Atoi(padding[0])
+		fromTop, _ := strconv.Atoi(strings.TrimSuffix(padding[1], ":"))
 
-	for i := range allStrings {
-		one := strings.Split(allStrings[i], " ")
-		fromLeft := strings.Split(one[2], ",")[0]
-		fromLeftInt, _ := strconv.Atoi(fromLeft)
-		fromTop := strings.Split(one[2], ",")[1]
-		fromTop = fromTop[0:len(fromTop)-1]
-		fromTopInt, _ := strconv.Atoi(fromTop)
-		width := strings.Split(one[3], "x")[0]
-		widthInt, _ := strconv.Atoi(width)
-		height := strings.Split(one[3], "x")[1]
-		heightInt, _ := strconv.Atoi(height)
+		dimensions := strings.Split(words[3], "x")
+		width, _ := strconv.Atoi(dimensions[0])
+		height, _ := strconv.Atoi(dimensions[1])
 
 		sumOfsquares := 0
-		for m := 0; m < heightInt; m++ {
-			for k := 0; k < widthInt; k++ {
-				if fabric[fromTopInt + m][fromLeftInt + k] == 1 {
+		for m := 0; m < height; m++ {
+			for k := 0; k < width; k++ {
+				if fabric[fromTop+m][fromLeft+k] == 1 {
 					sumOfsquares++
 				}
 			}
 		}
 
-		if sumOfsquares == widthInt * heightInt {
+		if sumOfsquares == width*height {
 			fmt.Println("Found!!!")
-			fmt.Println(one)
+			fmt.Println(words)
 		}
 
-
-
 	}
 
-
-	}
+}
